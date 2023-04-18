203aaf1c11c6bafcef176c5ebd5dbcf310f13e4f
Ariel <ariel@holamundo.me>
2023-04-18 08:55:58 +0200

    Inicio

--- page-aviso-legal.php
+++ page-aviso-legal.php
@@ -13,65 +13,78 @@
  */
 get_header();
 ?>
-	<div class="row aviso_cont">
+<div class="row aviso_cont">
 		<div class="col-sm-5 col-xs-10 col-xs-offset-1 aviso">
-			<h2>Aviso legal</h2>
-			<p>La visita a este sitio Web no implica que el usuario esté obligado a facilitar ninguna información.
-			En el caso de que el usuario facilite alguna información de carácter personal, los datos recogidos
-			en este sitio web serán tratados de forma leal y lícita con sujeción en todo momento a los
-			principios y derechos recogidos en el Reglamento (UE) 2016/679, de 27 de abril, General de Protección de Datos (RGPD) y demás normativa aplicable. </p>
-			<p>En cumplimiento de lo dispuesto en el artículo 13 del RGPD te informamos de lo siguiente:</p>
-
-			<h2>¿Qué hacemos con tus datos? </h2>
-<p>Los datos personales introducidos por el usuario se utilizan únicamente para fines de
-comunicación con el cliente, ya sea mediante el envío de boletines de noticias periódicos o
-comunicaciones comerciales. Estos datos nos ayudarán a ponernos en contacto contigo en caso
-de que tengas una incidencia o a contestarte si no has enviado una consulta mediante la sección
-provista para ello. En cualquier momento puedes solicitar que eliminemos o modifiquemos la
-información que nos has proporcionado, para ello sólo tienes que enviar un correo electrónico a
-la dirección info@agriverdes.com.
-Los usuarios deberán garantizar la exactitud, autenticidad y vigencia de los datos de carácter
-personal que hayan introducido en el sitio web. </p>
-
-			<h2>Redes sociales</h2>
-<p>A través de nuestra página web puede acceder a distintas redes sociales (Facebook, Twitter,
-Youtube, Linkedin, Pinterest, etc..) Los usuarios de éstas redes sociales deben conocer que este
-lugar es independiente de estas redes y las políticas de privacidad a aplicar a estos contenidos
-son las fijadas por los propietarios. </p>
-
-			<h2>Base jurídica y responsabilidades.</h2>
-<p>Agriverdes del Mediterráneo ETT, SL. está domiciliada en esta Calle Río de Plata, 10, bajo, 30700. Torre Pacheco, Murcia. con CIF numero B73074080. info@agriverdes.com</p>
-<p>
-Por ello, las presentes condiciones generales se rigen por la legislación española. Para cualquier
-litigio que pudiera surgir relacionado con el sitio web o la actividad que en él se desarrolla serán
-competentes Juzgados de ciudad, renunciando expresamente el usuario a cualquier otro fuero
-que pudiera corresponderle.
-Esta web no asume ninguna responsabilidad por eventuales daños que puedan sufrir los bienes
-del Usuario (incluso el ordenador), o por cualquier o pérdida o avería que sea consecuencia de la
-descarga o del uso del material presente. Se incluye la hipótesis de transmisión de virus
-informáticos.Tampoco nos hacemos responsables del contenido generados por nuestros
-usuarios, el cual será responsabilidad única del usuario que lo publica.
-En ningún caso, la existencia de enlaces a sitios externos da lugar a presuponer la existencia de
-acuerdos con los responsables o titulares de los mismos.</p>
+			<h2>POLÍTICA DE PRIVACIDAD Y PROTECCIÓN DE DATOS</h2>
+			<h2>¿QUIÉN ES EL RESPONSABLE DEL TRATAMIENTO DE TUS DATOS PERSONALES?</h2>
+				<ul>
+					<li><p>Denominación social: AGRIVERDES DEL MEDITERRANEO ETT, S.L.</p></li>
+					<li><p>Domicilio social: C/ RIO DE LA PLATA 10 30700 - TORRE-PACHECO (MURCIA)</p></li>
+					<li><p>CIF: B73074080</p></li>
+					<li><p>Dirección de correo electrónico: <a href="mailto:agriverdesett@agriverdes.com">agriverdesett@agriverdes.com</a></p></li>
+					<li><p>Teléfono: 968 58 73 36</p></li>
+					<li><p>Dirección web: <a href="https://agriverdes.com/">https://agriverdes.com/</a></p></li>
+				</ul>
+			<h2>¿CON QUÉ FINALIDAD TRATAMOS TUS DATOS PERSONALES?</h2>
+				<p>AGRIVERDES DEL MEDITERRANEO ETT trata tus datos principalmente para poder prestarte el servicio solicitado, detectar tus necesidades y poder adaptar nuestros productos a las mismas. Concretamente: </p>
+				<ul>
+					<li><p>Suscribir y ejecutar contratos contigo o la persona para quien trabajas. Para proporcionar personal y servicios a los clientes u otros usuarios de dichos empleados y servicios; obtener bienes y servicios de los proveedores; gestionar y administrar nuestras relaciones con los clientes, proveedores u otros usuarios de nuestros bienes y servicios; donde tú o la persona para quien trabajas podría ser el correspondiente cliente, usuario o proveedor de bienes/personal o servicios a estos efectos.</p></li>
+					<li><p>Ofrecerte, generalmente por correo electrónico o por teléfono, información sobre nuestros productos, personal y servicios y realizar prospección comercial.</p></li>
+					<li><p>Para administrar nuestro sitio web conforme a nuestros términos y para las operaciones internas, incluida la resolución de problemas y el análisis de datos, las pruebas, la investigación y con fines estadísticos y de realización de encuestas.</p></li>
+					<li><p>Para analizar el uso que haces de nuestro sitio web para mejorar tu experiencia de usuario y asegurarnos de que el contenido se presenta del modo más oportuno.</p></li>
+					<li><p>Para prevenir el uso inadecuado o fraudulento de nuestros servicios y mantener seguros y protegidos nuestro sitio web y otros sistemas.</p></li>
+				</ul>
+			<h2>¿DURANTE CUÁNTO TIEMPO CONSERVAMOS TUS DATOS PERSONALES?</h2>
+				<p>Los datos personales proporcionados se conservarán durante el periodo de tiempo necesario para el propósito o propósitos para los que originalmente se recogieron o hasta que se solicite su supresión por él interesado. Posteriormente se eliminarán o archivarán, salvo en la medida en que los necesitemos para seguir tratándolos para cumplir las obligaciones legales que nos corresponden o con otros fines legítimos y legales.</p>
+			<h2>¿CUÁL ES LA LEGITIMACIÓN PARA EL TRATAMIENTO DE TUS DATOS?</h2>
+				<p>La base legal para el tratamiento de tus datos radica en: </p>
+				<ul>
+					<li><p>La ejecución de un contrato en el que seas parte o para tomar medidas a tus solicitudes antes de formalizar dicho contrato con la finalidad indicada anteriormente.</p></li>
+					<li><p>El cumplimiento de obligaciones legales que nos afecten.</p></li>
+					<li><p>El cumplimiento de nuestros intereses legítimos para la finalidad indicada anteriormente.</p></li>
+					<li><p>El consentimiento del interesado para el tratamiento de sus datos personales.</p></li>
+				</ul>
+			<h2>¿A QUÉ DESTINATARIOS SE COMUNICARÁN TUS DATOS?</h2>
+				<p>Como norma general, no compartimos con nadie tus datos personales y te garantizamos su confidencialidad y seguridad.</p>
+			<h2>¿CUÁLES SON TUS DERECHOS?</h2>
+				<p>Tienes derecho a obtener confirmación sobre si en AGRIVERDES DEL MEDITERRANEO ETT estamos tratando datos personales que te conciernan, o no. Asimismo, tienes derecho a acceder a tus datos personales, así como a solicitar la rectificación de los datos inexactos o, en su caso, solicitar su supresión cuando, entre otros motivos, los datos que ya no sean necesarios para los fines que fueron recogidos.</p>
+				<p>En determinadas circunstancias, podrás solicitar la limitación del tratamiento de tus datos, en cuyo caso únicamente los conservaremos para el ejercicio o la defensa de reclamaciones.</p>
+				<p>Adicionalmente, en determinadas circunstancias y por motivos relacionados con tu situación particular, podrás oponerte al tratamiento de tus datos. AGRIVERDES DEL MEDITERRANEO ETT dejará de tratar los datos, salvo por motivos legítimos imperiosos, o el ejercicio o la defensa de posibles reclamaciones.</p>
 		</div>
-		<div class="col-sm-5 col-sm-offset-0 col-xs-10 col-xs-offset-1 aviso2">
-			<h2>Propiedad intelectual e industrial.</h2>
-<p>Los derechos de propiedad intelectual e industrial sobre las marcas, logos, y cualquier otro
-elemento susceptible de protección son propiedad de sus legítimos dueños. </p>
-<h2>Modificación de las Condiciones</h2>
-<p>Este sitio web se reservar el derecho a revisar y modificar las presentes Condiciones Generales
-en cualquier momento, las cuales tendrán que ser aceptados por los usuarios vez que se realicen
-cambios.</p>
-<h2>Aviso de cookies.</h2>
-<p>Este sitio web utiliza cookies para mejorar le experiencia de usuario. Estas cookies podrán ser
-generadas por el propio sitio web o por terceros como por ejemplo Google, Facebook u otras
-empresas que nos ayudan a que tu experiencia sea más completa. El usuario tiene la posibilidad
-de configurar su navegador para ser avisado de la recepción de cookies y para impedir su
-instalación en su equipo. Por favor consulte las instrucciones y manuales de su navegador para
-ampliar esta información.
-En todo caso las cookies tienen un carácter temporal con la única finalidad de hacer más eficaz
-su transmisión ulterior. En ningún caso se utilizará cookies para recoger información de carácter
-personal.</p>
+		<div class="col-sm-5 col-sm-offset-0 col-xs-10 col-xs-offset-1 aviso2">			
+			
+				<p>Asimismo, puedes ejercer el derecho a la portabilidad de los datos, así como retirar los consentimientos facilitados en cualquier momento, sin que ello afecte a la licitud del tratamiento basado en el consentimiento previo a su retirada.</p>
+				<p>Si deseas hacer uso de cualquiera de tus derechos puedes dirigirte a nosotros mediante los correos electrónicos indicados en el primer apartado o a través de los enlaces habilitados que encontrarás en los correos electrónicos y comunicaciones de AGRIVERDES DEL MEDITERRANEO ETT. Alternativamente, también puedes dirigirte a nosotros mediante correo postal dirigido a Atención al Usuario e indicando en el sobre "Protección de Datos". Recuerda facilitar la mayor información posible sobre tu solicitud, ten en cuenta que es posible que tengamos que pedirte más información para confirmar tu identidad.</p>
+				<p>Por último, te informamos que puedes dirigirte ante la Agencia Española de Protección de Datos y demás organismos públicos competentes para obtener más información sobre tus derechos, así como para cualquier reclamación derivada del tratamiento de tus datos personales.</p>
+			<h2>¿DE QUIÉN SON LOS DATOS PERSONALES QUE TRATAMOS?</h2>
+				<p>Podemos tratar datos personales sobre ti en los casos siguientes: </p>
+				<ul>
+					<li><p>Eres nuestro cliente o proveedor.</p></li>
+					<li><p>Usas nuestros productos o servicios.</p></li>
+					<li><p>Trabajas para uno de nuestros clientes o proveedores o para alguien que usa nuestros productos o servicios.</p></li>
+					<li><p>Eres alguien que quiere trabajar con nosotros o para nuestros clientes.</p></li>
+					<li><p>Eres alguien (o trabajas para alguien) a quien queremos vender o dar a conocer nuestros bienes o servicios. En este caso, es posible que hayamos obtenido tu información directamente de ti (por ejemplo, a través de nuestro sitio web) o de otra fuente.</p></li>
+				</ul>
+			<h2>¿QUÉ DATOS PERSONALES TRATAMOS?</h2>
+				<p>Podemos tratar datos personales sobre ti que hayamos obtenido de ti. Los datos personales sobre ti que tratamos pueden incluir la información personal que se indica a continuación: </p>
+				<ul>
+					<li><p>Nombre.</p></li>
+					<li><p>Para quién trabajas y tu función o departamento.</p></li>
+					<li><p>Formación o experiencia profesional o preferencias establecidas en el curriculum vitae que nos has facilitado.</p></li>
+					<li><p>Tu dirección, número de teléfono, dirección de correo electrónico u otra información de contacto (esta información puede referirse a tu trabajo o a ti personalmente, dependiendo de la naturaleza de nuestra relación contigo o la persona para quien trabajas).</p></li>
+				</ul>
+			<h2>INFORMACIÓN QUE OBTENEMOS AL COMUNICARNOS CONTIGO</h2>
+				<ul>
+					<li><p>Información sobre ti que nos das al comunicarte con nosotros por teléfono, correo electrónico, a través de nuestro sitio web, las redes sociales o de otro modo.</p></li>
+					<li><p>Información que nos das o que obtenemos cuando utilizas nuestro sitio web, obtienes bienes o servicios o te suscribes a ellos, nos proporcionas bienes o servicios, preguntas sobre un producto, haces un pedido o contactas con nosotros para notificar un problema, o haces cualquiera de estas cosas en nombre de la persona para quien trabajas.</p></li>
+					<li><p>Otra información relativa a ti que necesitemos tratar para suscribir o ejecutar un contrato contigo o la persona para quien trabajas (por ejemplo, información sobre tu capacidad de trabajar e información obtenida de entidades que ofrecen referencias crediticias, en el caso de que esto sea necesario para poder llevar a cabo las comprobaciones pertinentes en relación con los contratos contigo u otra persona para quien trabajes o con quien tengas una relación de otra naturaleza).</p></li>
+				</ul>
+			<h2>INFORMACIÓN QUE OBTENEMOS DE TI CUANDO UTILIZAS NUESTRO SITIO WEB</h2>
+				<ul>
+					<li><p>Información técnica, como la dirección de Protocolo de Internet (IP) que se ha utilizado para conectar tu ordenador a Internet, tu información de inicio de sesión, tipo y versión del navegador, configuración de la zona horaria, tipos y versiones de los complementos del navegador, sistema operativo y plataforma.</p></li>
+					<li><p>Información sobre tu visita, como las direcciones URL (localizadores uniformes de recursos), información de clickstream (tráfico) desde y hasta nuestro sitio web, y a través del mismo (incluidas la hora y la fecha), productos que has buscado o visto, tiempos de respuesta de las páginas, errores de descarga, duración de las visitas a determinadas páginas, información sobre la interacción con las páginas (como desplazamientos, clics y pasar el ratón por encima de algo), métodos usados para navegar fuera de la página y cualquier número de teléfono utilizado para llamar a nuestro número de atención al cliente o cuenta de las redes sociales usada para conectar con nuestro equipo de atención al cliente.</p></li>
+					<li><p>Al enviar un formulario desde nuestras secciones de contacto, almacenamos los datos enviados en el mismo: nombre, apellidos, empresa/organización, email, teléfono, ciudad y mensaje enviado.</p></li>
+				</ul>
 		</div>
 	</div>
 <?php
